a6909c4d0ea2eb6ef0a03ec9ffae6dbac3c6818c
TaipeiGuru <78506727+TaipeiGuru@users.noreply.github.com>
2023-02-17 12:23:19 -0800

    updated heap

--- heap.cpp
+++ heap.cpp
@@ -4,31 +4,37 @@
 using namespace std;
 
 void insertNum(int* maxHeap, int num);
+void displayHeap(int* maxHeap);
 
 int main() {
   int maxHeap[101];
   maxHeap[0] = -1;
-  for(int i = 1, i < 101; i++) {
-    maxHeap[i] = NULL; 
+  for(int i = 1; i < 101; i++) {
+    maxHeap[i] = 0; 
   }
   char input[20];
+
+  cout << "Welcome to Heap!" << endl;
   
   bool active = true;
   while(active == true) {
-    cout << "Welcome to Heap!" << endl;
-    cout << "\nYour commands are \"INSERT,\" \"REMOVE LARGEST,\" \"REMOVE ALL,\" \"DISPLAY,\" and \"QUIT.\"" << endl;
-    cin.getline(input, 20);
+    cout << "\nYour commands are \"INSERT,\" \"REMOVE_LARGEST,\" \"REMOVE_ALL,\" \"DISPLAY,\" and \"QUIT.\"" << endl;
+    cin >> input;
+    cin.clear();
+    cin.ignore(10000, '\n');
     if(strcmp(input, "INSERT") == 0) {
       cout << "Choose your insert method: \"INPUT\" \"FILE\"" << endl;
       cin >> input;
       cin.clear();
       cin.ignore(10000, '\n');
-      if((strcmp(input, "INPUT") == 0) {
+      if(strcmp(input, "INPUT") == 0) {
         int num;
         cout << "What number would you like to add? It must be between 1-1000." << endl;
         cin >> num;
+	cin.clear();
+	cin.ignore(10000, '\n');
         insertNum(maxHeap, num);
-      } else if((strcmp(input, "FILE") == 0) {
+      } else if(strcmp(input, "FILE") == 0) {
                
       } else {
         cout << "Sorry, invalid input." << endl; 
@@ -38,7 +44,7 @@ int main() {
     } else if(strcmp(input, "REMOVE ALL") == 0) {
       
     } else if(strcmp(input, "DISPLAY") == 0) {
-               
+      displayHeap(maxHeap);
     } else if(strcmp(input, "QUIT") == 0) {
       active = false; 
     } else {
@@ -49,9 +55,17 @@ int main() {
 }
 
 void insertNum(int* maxHeap, int num) {
+  int counter = 1;
+  while(maxHeap[counter] != 0) {
+    counter++;
+  }
+  maxHeap[counter] = num;
+}
+
+void displayHeap(int* maxHeap) {
   for(int i = 1; i < 101; i++) {
-    if(maxHeap[i] == NULL) {
-      maxHeap[i] = num;
+    if(maxHeap[i] != 0){
+      cout << maxHeap[i] << endl;
     }
   }
 }
