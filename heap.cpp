f538a3c901004a268ad50ba838f9a6498fd1a92d
TaipeiGuru <78506727+TaipeiGuru@users.noreply.github.com>
2023-02-17 12:07:23 -0800

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -8,6 +8,9 @@ void insertNum(int* maxHeap, int num);
 int main() {
   int maxHeap[101];
   maxHeap[0] = -1;
+  for(int i = 1, i < 101; i++) {
+    maxHeap[i] = NULL; 
+  }
   char input[20];
   
   bool active = true;
@@ -16,11 +19,24 @@ int main() {
     cout << "\nYour commands are \"INSERT,\" \"REMOVE LARGEST,\" \"REMOVE ALL,\" \"DISPLAY,\" and \"QUIT.\"" << endl;
     cin.getline(input, 20);
     if(strcmp(input, "INSERT") == 0) {
+      cout << "Choose your insert method: \"INPUT\" \"FILE\"" << endl;
+      cin >> input;
+      cin.clear();
+      cin.ignore(10000, '\n');
+      if((strcmp(input, "INPUT") == 0) {
+        int num;
+        cout << "What number would you like to add? It must be between 1-1000." << endl;
+        cin >> num;
+        insertNum(maxHeap, num);
+      } else if((strcmp(input, "FILE") == 0) {
                
+      } else {
+        cout << "Sorry, invalid input." << endl; 
+      }
     } else if(strcmp(input, "REMOVE LARGEST") == 0) {
                
     } else if(strcmp(input, "REMOVE ALL") == 0) {
-      active = false;
+      
     } else if(strcmp(input, "DISPLAY") == 0) {
                
     } else if(strcmp(input, "QUIT") == 0) {
@@ -33,5 +49,9 @@ int main() {
 }
 
 void insertNum(int* maxHeap, int num) {
-  
+  for(int i = 1; i < 101; i++) {
+    if(maxHeap[i] == NULL) {
+      maxHeap[i] = num;
+    }
+  }
 }
